2e17a5f37449e1a52850fc7a12df4e258dd7b965
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 14:43:50 +0000

    updated lambda/config.js.

---
+++ lambda/config.js
@@ -0,0 +1,39 @@
+module.exports.INITIAL_LIVES = 5;
+
+module.exports.WORDS = [
+    'ventilador',
+    'parede',
+    'porta',
+    'celular',
+    'rato',
+    'macarrao',
+];
+
+module.exports.VALID_LETTERS = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'X',
+    'Z',
+    'W',
+    'Y',
+];
