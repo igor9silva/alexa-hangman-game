ef32ce77c4f7ed18834f9d8ab68c81382f00fd14
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 22:25:40 +0000

    updated lambda/VUI.js.

--- lambda/VUI.js
+++ lambda/VUI.js
@@ -19,18 +19,19 @@ module.exports.MESSAGES = {
         'Hello, you are in the Hangman Game!': `Hello, you are in the Hangman Game!`,
         'The game has started!': `The game has started!`,
         'The word has {0} letters.': `The word has {0} letters.`,
-        'You have {0} {1}.': `Você tem {0} {1}.`,
+        'You have {0} {1}.': `You have {0} {1}.`,
     },
     'pt': {
-        
+
         // words
         'life': 'vida',
         'lives': 'vidas',
-        
+
         // 1
         'Hello, you are in the Hangman Game!': `Olá, você está no Jogo da Forca!`,
         'The game has started!': `O jogo já começou!`,
         'The word has {0} letters.': `A palavra possui {0} letras.`,
+        'You have {0} {1}.': `Você tem {0} {1}.`,
     },
 }
 
