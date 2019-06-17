b0167585edb4ff8bf73506da77bd21495139e638
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 23:08:10 +0000

    updated lambda/handlers/getStatusIntentHandler.js.

--- lambda/handlers/getStatusIntentHandler.js
+++ lambda/handlers/getStatusIntentHandler.js
@@ -16,7 +16,7 @@ module.exports = {
     
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-             && handlerInput.requestEnvelope.request.intent.name === 'GetStatusIntent';
+            && handlerInput.requestEnvelope.request.intent.name === 'GetStatusIntent';
     },
     
     async handle(handlerInput) {
