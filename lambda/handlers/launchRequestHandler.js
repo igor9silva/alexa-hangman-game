0ee32f361e938027fa189dab918a4e22d6978f69
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 15:00:01 +0000

    updated lambda/handlers/launchRequestHandler.js.

---
+++ lambda/handlers/launchRequestHandler.js
@@ -0,0 +1,43 @@
+
+// Constants
+const { WORDS } = require('./config');
+
+const LaunchRequestHandler = {
+    
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    
+    async handle(handlerInput) {
+
+        // shuffle a word
+        const chosenWord = WORDS[randomIndex(0, WORDS.length)];
+
+        // get session attributes
+        const attributes = await handlerInput.attributesManager.getSessionAttributes();
+
+        // create initial params
+        attributes.word = chosenWord.toUpperCase();
+        attributes.triedLetters = [];
+
+        // save attributes
+        handlerInput.attributesManager.setSessionAttributes(attributes);
+
+        // say hello, the chosen word length and how
+        // many lives the user have
+        const sentences = [
+            `Olá, você está no Jogo da Forca!`,
+            `O jogo já começou!`,
+            `A palavra possui ${chosenWord.length} letras.`,
+            `Você tem ${INITIAL_LIVES} vidas.`,
+            `Chute uma letra.`,
+        ];
+
+        return handlerInput.responseBuilder
+                .speak(sentences.join(' '))
+                .reprompt('Diga alguma letra...')
+                .getResponse();
+    }
+};
+
+module.exports.LaunchRequestHandler = LaunchRequestHandler;
