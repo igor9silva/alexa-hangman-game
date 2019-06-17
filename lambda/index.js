dc0b7a93f11ee8bb049e8812bcbc2cc626cbd50a
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 15:09:09 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -35,66 +35,7 @@ const {
 
 const LaunchRequestHandler = require('./handlers/launchRequestHandler');
 const SuggestLetterIntentHandler = require('./handlers/suggestLetterIntentHandler');
-
-
-
-
-
-
-
-const GetStatusIntentHandler = {
-    
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'GetStatusIntent';
-    },
-    
-    async handle(handlerInput) {
-        
-        const attributes = await handlerInput.attributesManager.getSessionAttributes();
-        const { word, triedLetters } = attributes;
-        
-        const lifeCount = countLives(word, triedLetters);
-        const missingCount = countMissingLetters(word, triedLetters);
-        
-        console.log('lifeCount:', lifeCount);
-        console.log('missingCount:', missingCount);
-        console.log('triedLetters:', triedLetters.join(', '));
- 
-        let speechText = '';
-        
-        // say life count
-        if (lifeCount > 1) {
-            speechText += p(`Restam ${lifeCount} vidas.`);
-        } else if (lifeCount === 1) {
-            speechText += p(`Resta ${lifeCount} vida.`);
-        }
-        
-        // say missing count
-        if (missingCount > 1) {
-            speechText += p(`Faltam ${missingCount} letras.`);
-        } else if (missingCount === 1) {
-            speechText += p(`Falta 1 letra.`);
-        }
-
-        // spell out letters (say "empty" if hasn't been guessed)
-        speechText += p(`Vou falar casa por casa ${triedLetters}:`);
-        for (let letter of word) {
-            if (hasBeenGuessed(letter, triedLetters)) {
-                speechText += p(letter);
-            } else {
-                speechText += p('vazio');
-            }
-        }
-
-        // const speechText = `Vou listar os espaços: vazio, e, n, vazio, i, l, a, vazio, vazio, vazio. A palavra possui 10 letras. Você ainda tem 3 vidas.`;
-
-        return handlerInput.responseBuilder
-                .speak(speechText)
-                .reprompt('Diga alguma letra...')
-                .getResponse();
-    }
-};
+const GetStatusIntentHandler = require('./handlers/GetStatusIntentHandler');
 
 // This handler acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
