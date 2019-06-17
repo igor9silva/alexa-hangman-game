01659f8d225d65d04f71dfb20a422407bd875dca
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 22:04:13 +0000

    updated lambda/config.js.

--- lambda/config.js
+++ lambda/config.js
@@ -10,30 +10,14 @@ module.exports.WORDS = [
 ];
 
 module.exports.VALID_LETTERS = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'X',
-    'Z',
-    'W',
-    'Y',
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 
+    'S', 'T', 'U', 'V', 'X', 'Z',
+    'W', 'Y',
 ];
+
+module.exports.SUPPORTED_LANGUAGES = {
+    'en': ['US', 'UK', 'AU', 'CA', 'IN'],
+    'pt': ['BR', 'PT'],
+};
