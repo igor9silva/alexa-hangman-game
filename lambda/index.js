190f8ed00d0a145ab7c450c57a87fb15fe8b417e
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 14:53:20 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -206,44 +206,6 @@ function p(text) {
     return `<p>${text}</p>\n`
 }
 
-// ACTUAL GAME LOGIC
-
-/// boolean
-/// return if letter has already been guessed
-function hasBeenGuessed(letter, triedLetters) {
-    console.log('hasBeenGuessed:', letter, triedLetters, triedLetters.includes(letter))
-    return triedLetters.includes(letter);
-}
-
-/// int
-/// return the hit count (how many occurrencies of `letter` in `word`)
-function countHits(letter, word) {
-    return word.split(letter).length - 1; // `letter` occurrencies in `word`
-}
-
-/// int
-/// return the current life count
-function countLives(word, triedLetters) {
-
-    const failedAttempts = triedLetters.reduce((count, letter) => {
-        return count + (word.includes(letter) ? 0 : 1);
-    }, 0);
-    
-    return INITIAL_LIVES - failedAttempts;
-}
-
-/// int
-/// return how many missing letters there are
-function countMissingLetters(word, triedLetters) {
-
-    // sum, for each letter, how many occurrencies of it there are
-    const totalHitCount = triedLetters.reduce((count, letter) => {
-        return count + (countHits(letter, word));
-    }, 0);
-    
-    return word.length - totalHitCount;
-}
-
 // This handler acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
