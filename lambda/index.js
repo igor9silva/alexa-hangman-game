d302e19677f188f0b929dc22eb7c43e9de42437c
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 00:16:56 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -235,5 +235,5 @@ exports.handler = Alexa.SkillBuilders.custom()
         IntentReflectorHandler) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
     .addErrorHandlers(
         ErrorHandler)
-    .withPersistenceAdapter(persistenceAdapter)
+    // .withPersistenceAdapter(persistenceAdapter)
     .lambda();
