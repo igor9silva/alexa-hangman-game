e1a425ad2d05a5c1d4ea2937ca9e6ac40d5e666a
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 15:02:15 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -16,6 +16,14 @@ const {
     p,
 } = require('./util');
 
+// Game Logic
+const {
+    hasBeenGuessed,
+    countHits,
+    countLives,
+    countMissingLetters,
+} = require('./game');
+
 // Intent Handlers
 const {
     HelpIntentHandler,
@@ -25,51 +33,11 @@ const {
     ErrorHandler,
 } = require('./handlers/defaultHandlers');
 
-// Game Logic
-const {
-    hasBeenGuessed,
-    countHits,
-    countLives,
-    countMissingLetters,
-} = require('./game')
+const LaunchRequestHandler = require('./handlers/launchRequestHandler')
 
-const LaunchRequestHandler = {
-    
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-    },
-    
-    async handle(handlerInput) {
 
-        // shuffle a word
-        const chosenWord = WORDS[randomIndex(0, WORDS.length)];
 
-        // get session attributes
-        const attributes = await handlerInput.attributesManager.getSessionAttributes();
 
-        // create initial params
-        attributes.word = chosenWord.toUpperCase();
-        attributes.triedLetters = [];
-
-        // save attributes
-        handlerInput.attributesManager.setSessionAttributes(attributes);
-
-        // say hello, the chosen word length and how
-        // many lives the user have
-        const sentences = [
-            `Olá, você está no Jogo da Forca!`,
-            `O jogo já começou!`,
-            `A palavra possui ${chosenWord.length} letras.`,
-            `Você tem ${INITIAL_LIVES} vidas.`,
-            `Chute uma letra.`,
-        ];
-
-        return handlerInput.responseBuilder
-                .speak(sentences.join(' '))
-                .reprompt('Diga alguma letra...')
-                .getResponse();
-    }
-};
 
 const SuggestLetterIntentHandler = {
     
