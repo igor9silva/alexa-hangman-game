04078611a47741fd8159dde1892e3abe29169937
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 22:09:14 +0000

    updated lambda/VUI.js.

--- lambda/VUI.js
+++ lambda/VUI.js
@@ -1,6 +1,6 @@
 
 // Constants
-const { 
+const {
     WORDS,
     SUPPORTED_LANGUAGES
 } = require('./config');
@@ -15,52 +15,4 @@ const MESSAGES = {
     'pt': {
         
     }
-}
-
-function localeIsValid(locale) {
-
-    if (typeof(locale) !== 'string') {
-        return false;
-    }
-
-    if (locale.length !== 5) {
-        return false;
-    }
-
-    const [language, region] = locale.split('_');
-
-    if (language === undefined || language.length !== 2) {
-        return false;
-    }
-
-    if (region === undefined || region.length !== 2) {
-        return false;
-    }
-
-    if (!Object.keys(SUPPORTED_LANGUAGES).includes(language.toLowerCase())) {
-        return false;
-    }
-
-    const regions = SUPPORTED_LANGUAGES[language];
-
-    if (!regions.includes(region.toUpperCase())) {
-        return false;
-    }
-
-    return true;
-}
-
-module.exports.localizedMessage = function localizedMessage(key, locale) {
-    
-    if (!localeIsValid(locale)) {
-        throw "LocaleNotSupported";
-    }
-    
-    const [language] = locale.split('_');
-    
-    return MESSAGES[language][key];
-}
-
-module.exports.localizedRandomeWord = function localizedRandomeWord(locale) {
-    const chosenWord = WORDS[randomIndex(0, WORDS.length)];
 }
