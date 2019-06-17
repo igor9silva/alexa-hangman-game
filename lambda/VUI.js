5bc7019ef9942aa1087458db687cb300b23d5c65
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 22:20:16 +0000

    updated lambda/VUI.js.

--- lambda/VUI.js
+++ lambda/VUI.js
@@ -1,14 +1,32 @@
 
+// `Olá, você está no Jogo da Forca!`,
+//             `O jogo já começou!`,
+//             `A palavra possui ${chosenWord.length} letras.`,
+//             `Você tem ${INITIAL_LIVES} vidas.`,
+//             `Chute uma letra.`,
+//         ];
+
+//         return handlerInput.responseBuilder
+//                 .speak(sentences.join(' '))
+//                 .reprompt('Diga alguma letra...')
+
+
 module.exports.MESSAGES = {
     'en' : {
-        
+        'Hello, you are in the Hangman Game!': `Hello, you are in the Hangman Game!`,
+        'The game has started!': `The game has started!`,
+        'The word has {0} letters.': `The word has {0} letters.`,
     },
     'pt': {
-        
-    }
+        'Hello, you are in the Hangman Game!': `Olá, você está no Jogo da Forca!`,
+        'The game has started!': `O jogo já começou!`,
+        'The word has {0} letters.': `A palavra possui {0} letras.`,
+    },
 }
 
 module.exports.WORDS = {
+    'en': [
+    ],
     'pt': [
         'ventilador',
         'parede',
@@ -17,6 +35,4 @@ module.exports.WORDS = {
         'rato',
         'macarrao',
     ],
-    'en': [
-    ]
 }
