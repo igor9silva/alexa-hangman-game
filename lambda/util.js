2d0edd968cd9ea4f94cea42fff9899050940b6b2
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 14:50:14 +0000

    updated lambda/util.js.

--- lambda/util.js
+++ lambda/util.js
@@ -4,14 +4,17 @@ const s3SigV4Client = new AWS.S3({
     signatureVersion: 'v4'
 });
 
+// get pre-signed S3 URL
 module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
 
     const bucketName = process.env.S3_PERSISTENCE_BUCKET;
+    
     const s3PreSignedUrl = s3SigV4Client.getSignedUrl('getObject', {
         Bucket: bucketName,
         Key: s3ObjectKey,
         Expires: 60*1 // the Expires is capped for 1 minute
     });
+
     console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`);
     return s3PreSignedUrl;
 
