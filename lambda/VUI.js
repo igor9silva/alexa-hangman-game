ce0169aeea3cf2dedf1b1e3e5e7f4d8aa13dd1da
VoiceHubCodeCommitRole/VoiceHubCodeCommitRole <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2019-06-17 21:57:33 +0000

    updated lambda/VUI.js.

---
+++ lambda/VUI.js
@@ -0,0 +1,51 @@
+
+const LANGUAGES = {
+    'en': ['US', 'UK', 'AU', 'CA', 'IN'],
+    'pt': ['BR', 'PT'],
+};
+
+const MESSAGES = {
+    'en' : {
+        
+    },
+    'pt': {
+        
+    }
+}
+
+function localeIsValid(locale) {
+    
+    if (typeof(locale) !== 'string') {
+        return false;
+    }
+    
+    if (locale.length !== 5) {
+        return false;
+    }
+    
+    const [language, region] = locale.split('_');
+    
+    if (language === undefined || language.length !== 2) {
+        return false;
+    }
+    
+    if (region === undefined || region.length !== 2) {
+        return false;
+    }
+    
+    if (!Object.keys(LANGUAGES).includes(language.toLowerCase())) {
+        return false;
+    }
+    
+    const regions = LANGUAGES[language];
+    
+    if (!regions.includes(region)) {
+        return false;
+    }
+    
+    return true;
+}
+
+function localizedMessage(key) {
+    
+}
